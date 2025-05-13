fed2ebd03c5b3b9dda6b2478762cb860065ac749
LeiLoliLu <leiregose@gmail.com>
2025-05-13 15:15:59 +0200

    Fix non versioned files

---
+++ GodotTest/test/Objects/Baño/inodoro.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	DialogueManager.process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	
+	
+func showInteraction(_player):
+	var interaction = DialogueManager.show_dialogue_balloon(load("res://Dialogue/baño1.dialogue"), "inodoro")
+	interaction.process_mode=Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = true
+		
+	DialogueManager.dialogue_ended.connect(_unpause)
+	
+	
+func _unpause(_resource):
+	get_tree().paused = false
+	DialogueManager.dialogue_ended.disconnect(_unpause)
+	
---
+++ GodotTest/test/Objects/UI/MasterAudio/sound_effect_player.gd
@@ -0,0 +1,11 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.volume_db=-20
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
